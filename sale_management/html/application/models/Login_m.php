f19547302dee2bc6e3cb60dfd9259692d6375c55
faithinker <faithinker12@gmail.com>
2020-01-03 18:41:54 +0900

    Cinema

--- sale_management/html/application/models/Login_m.php
+++
@@ -1,8 +0,0 @@
-<?
-  class Login_m extends CI_Model{     // 모델 클래스 선언
-    function getrow($uid,$pwd){
-        $sql = "select * from member where uid8='$uid' and pwd8='$pwd'";
-        return $this->db->query($sql)->row();
-    }
-  }
-?>
